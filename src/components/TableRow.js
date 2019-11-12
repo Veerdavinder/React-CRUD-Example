afc15734a948b48a0e5cc59ce38cc46990f5246c
Veer_Davinder <birdevinder1705@gmail.com>
2019-11-11 19:26:43 -0500

    Implemented delete functionality

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -1,6 +1,18 @@
 import React,{Component} from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 class TableRow extends Component{
+
+    constructor(props){
+        super(props);
+        this.delete = this.delete.bind(this);
+    }
+    delete(){
+        axios.get('http://localhost:4000/business/delete/'+this.props.obj._id)
+            .then(console.log('Deleted'))
+            .catch(err=>console.log(err));
+            window.location.reload(false);
+    }
     render(){
         return(
             <tr>
@@ -17,7 +29,7 @@ class TableRow extends Component{
                     <Link to={"/edit/"+this.props.obj._id} className="btn btn-primary">Edit</Link>
                 </td>
                 <td>
-                    <button className="btn btn-danger">Delete</button>
+                    <button onClick={this.delete} className="btn btn-danger">Delete</button>
                 </td>
             </tr>   
         )
